59dd569fcd3c2ff7cc7689d864c3437dbc848523
Luis Henrique <luishng123@gmail.com>
2016-05-11 21:31:29 -0300

    Creating provider model

---
+++ src/model/Provider.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Provider {
+	private String conpany;
+	private String salesman;
+	private String salesmanPhone;
+	
+	public Provider() {
+		
+	}
+	
+	public String getConpany() {
+		return conpany;
+	}
+
+	public void setConpany(String conpany) {
+		this.conpany = conpany;
+	}
+
+	public String getSalesman() {
+		return salesman;
+	}
+
+	public void setSalesman(String salesman) {
+		this.salesman = salesman;
+	}
+
+	public String getSalesmanPhone() {
+		return salesmanPhone;
+	}
+
+	public void setSalesmanPhone(String salesmanPhone) {
+		this.salesmanPhone = salesmanPhone;
+	}
+}
