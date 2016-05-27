5aa4773c13a0815cbf17ffce79f2b6cf00fcd762
Laercio Silva <laerciossjr@gmail.com>
2016-05-27 20:56:53 -0300

    Add models

---
+++ src/model/Key.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Key extends Product{
+	public Key(String name, String description, String location,
+			int quantityMin){
+		super(name,description,location, quantityMin);
+	}
+}
---
+++ src/model/Part.java
@@ -0,0 +1,33 @@
+package model;
+
+
+
+public class Part extends Product {
+	private String brand;
+	private String modelCar;
+	
+	public Part(String name, String description, String location,
+			int quantityMin, String brand, String modelCar) {
+		super(name,description,location, quantityMin);
+		this.brand = brand;
+		this.modelCar = modelCar;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModelCar() {
+		return modelCar;
+	}
+
+	public void setModelCar(String modelCar) {
+		this.modelCar = modelCar;
+	}
+	
+	
+}
