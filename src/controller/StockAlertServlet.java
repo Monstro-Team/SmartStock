21ba54f684e80abfb3c8bcbcb99332c739023653
Laercio Silva <laerciossjr@gmail.com>
2016-06-08 21:26:21 -0300

    Stock alert is working

--- src/controller/StockAlertServlet.java
+++ src/controller/StockAlertServlet.java
@@ -13,7 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.ProductDAO;
+import dao.StockDAO;
 import model.Product;
+import model.Stock;
 
 @WebServlet(urlPatterns = {"/StockAlertServlet"},
 initParams = {
@@ -23,15 +25,29 @@ public class StockAlertServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6248157882432345603L;
 	private ProductDAO productDAO;
+	private StockDAO stockDAO;
 	
 	protected void service (HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
 		ArrayList<Product> products = new ArrayList<Product>();
-
+		int total;
 		try {
 			productDAO = new ProductDAO();
-			products = productDAO.getAllProductsLowInStock();
+			stockDAO = new StockDAO();
+			
+			for(Product product: productDAO.getAllProducts()){
+				total =0;
+				for(Stock stock: stockDAO.getAllStock()){
+					if(stock.getIdProduct() == product.getId()){
+						total = total + stock.getQuantity();
+					}
+				}
+				if(total < product.getQuantityMin()){
+					product.setQuantity(total);
+					products.add(product);
+				}
+			}
 		} catch (SQLException e) {
 			request.setAttribute("error", "Erro no banco de dados!");
 		}
